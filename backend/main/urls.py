67e072d0c92adede8b01ab2facb447d23fe9a70c
a1masssss <almas.issakov.t@gmail.com>
2025-06-10 11:37:18 +0500

    remove hardcoded urls

--- backend/main/urls.py
+++ backend/main/urls.py
@@ -11,17 +11,11 @@ urlpatterns = [
     path('playlists/<uuid:playlist_uuid>/', PlaylistDetailAPIView.as_view(), name="get_playlist"),
     path('videos/<uuid:video_uuid>/', VideoDetailAPIView.as_view(), name="get_video"),
 
-
     path('my-courses/', MyCoursesAPIView.as_view(), name="my_courses"),
 
-
-    
     path('summary-chatbot/', SummaryChatbotAPIView.as_view(), name="summary_chatbot"),
     path('flashcards/', GenerateFlashCardsView.as_view(), name="flashcards"),   
     path('mindmap/', GenerateMindMapView.as_view(), name="mindmap"),
-
-
-
 
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
